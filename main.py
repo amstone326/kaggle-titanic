b35509977c99c6327a681ad67c04df3bdcbf9931
ALIZA STONE <amstone326@gmail.com>
2018-01-07 22:04:04 -0500

    more progress

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import pandas as pd # https://pandas.pydata.org/pandas-docs/stable/
 import numpy as np
 import random as rnd
 
+# visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 train_df = pd.read_csv('./data/train.csv')
 test_df = pd.read_csv('./data/test.csv')
 combine = [train_df, test_df]
@@ -14,13 +18,27 @@ combine = [train_df, test_df]
 # print((train_df.loc[(train_df['Parch'] == 0) & (train_df['SibSp'] == 0)]).shape)
 # print((train_df.loc[train_df['Sex'] == 'male']).shape)
 
-print(train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False))
-print('-'*30)
-print(train_df[['Sex', 'Survived']].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False))
-print('-'*30)
-print(train_df[['SibSp', 'Survived']].groupby(['SibSp'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+# print(train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+# print('-'*30)
+# print(train_df[['Sex', 'Survived']].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+# print('-'*30)
+# print(train_df[['SibSp', 'Survived']].groupby(['SibSp'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+# print('-'*30)
+# print(train_df[['Parch', 'Survived']].groupby(['Parch'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+# print('-'*30)
+# print(train_df[['Embarked', 'Survived']].groupby(['Embarked'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+# print('-'*30)
+# Shows how many people were in each (Pclass, Embarked) grouping (to show if there's a correlation between Pclass and Embarked)
+# print(train_df[['Embarked', 'Pclass', 'PassengerId']].groupby(['Embarked', 'Pclass'], as_index=False).count())
+
+print(train_df[['Embarked', 'Sex', 'Survived']].groupby(['Embarked', 'Sex'], as_index=False).mean())
+
+# g = sns.FacetGrid(train_df, col='Survived')
+# g.map(plt.hist, 'Age', bins=20)
+# plt.show()
 
 
+# ------------------------------------------------------------------------------
 # USEFUL pandas METHODS TO USE ON DATA FRAMES
 
 # For each column, gives the count, mean, stdev, min, max, and percentile values (default is 25, 50, 75 but can
